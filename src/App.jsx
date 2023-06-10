1b3b85fd8fd369db84ec6e96ea7e2b02483bba80
kkris26 <krisnuartha09@gmail.com>
2023-06-11 06:05:34 +0800

    update error pada main.jsx dan Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-
-import { useState } from "react";
-import "./App.css";
-import Navigation from "./components/navigation/Navigation";
-import Container from "react-bootstrap/esm/Container";
-import Hero from "./components/Hero";
-import DetailOlahraga from "./components/detailOlahraga";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
@@ -12,27 +5,27 @@ import AllDiet from "./pages/AllDiet";
 import DetailsPageDiet from "./components/RekomendasiDIet/DetailsPageDiet";
 import PageOlahragaAll from "./pages/PageOlahragaAll";
 import PageMakananAll from "./pages/PageMakananAll";
-
+import Navigation from "./components/navigation/Navigation";
+import Container from "react-bootstrap/Container";
+import Hero from "./components/Hero";
+import DetailOlahraga from "./components/detailOlahraga";
+import "./App.css";
 
 function App() {
   return (
-
-    <>
+    <Router>
       <Navigation />
       <Container>
         {/* <Hero/> */}
         <DetailOlahraga />
       </Container>
-    </>
-
-    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/alldiet" element={<AllDiet />} />
         <Route path="/makanan-all" element={<PageMakananAll />} />
         <Route path="/olahraga-all" element={<PageOlahragaAll />} />
-        <Route path="/details/:id" element={<DetailsPageDiet />} />{" "}
+        <Route path="/details/:id" element={<DetailsPageDiet />} />
         {/* Tambahkan route untuk details */}
       </Routes>
     </Router>
