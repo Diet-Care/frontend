8bc0001f694aad73eeaae3385128541ed1fffadc
MustafaSuryaSyahputra <mustafasuryasyahputra@gmail.com>
2023-06-10 16:24:46 +0700

    update hero edukasi dan penambahan navigation

--- src/components/Edukasi/Edukasi.jsx
+++ src/components/Edukasi/Edukasi.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import EdukasiMakanan from "./Makanan/EdukasiMakanan";
 import HeroEdukasi from "./Hero/Hero";
+import Navigation from "../Navigation/Navigation";
 
 const Edukasi = () => {
   return (
     <>
+        <Navigation/>
         <HeroEdukasi/>
     <div className="container">
       <div class="row gap-0 g-4 content mt-lg-0 mt-2">
--- src/components/Edukasi/Hero/Hero.jsx
+++ src/components/Edukasi/Hero/Hero.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "../../../style/heroedukasi.css";
-import { Container } from "react-bootstrap";
+import "../../../style/heroedukasi.css";1   
 
 const HeroEdukasi = () => {
   return (
@@ -13,7 +12,8 @@ const HeroEdukasi = () => {
       width:'100%'
     }}
     >
-      
+      <p id="title" className="mt-lg-0 mt-3">Apa itu Gizi Seimbang?</p>
+      <p id="description" className="mb-lg-5 mb-4 mt-lg-3 mt-0 col-6">Gizi seimbang adalah susunan makanan sehari-hari yang mengandung zat gizi dalam jenis dan jumlah yang sesuai dengan kebutuhan tubuh yaitu jenis kelamin, umur dan status kesehatan. Pola makan tidak seimbang meningkatkan risiko kekurangan atau kelebihan gizi yang dapat berdampak negatif pada kesehatan</p>
     </div>
   );
 };
