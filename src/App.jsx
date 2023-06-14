9da5607a8c21ea2d17b4362b5c4b3964e5deb2b4
kkris26 <krisnuartha09@gmail.com>
2023-06-14 15:10:13 +0800

    update merge home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import About from "./pages/About";
 import AllDiet from "./pages/AllDiet";
 import DetailsPageDiet from "./components/Details Page/DetailsPage";
 import PageOlahragaAll from "./pages/PageOlahragaAll";
@@ -10,47 +9,30 @@ import AdminPage from "./pages/AdminPage";
 import EditMakanan from "./components/Admin/EditData/EditMakanan";
 import EditOlahraga from "./components/Admin/EditData/EditOlahraga";
 
-
-import Navigation from "../src/components/Navigation";
 import Footer from "./components/Footer";
-import Container from "react-bootstrap/Container";
-import Hero from "./components/Hero";
-import DetailOlahraga from "./components/detailOlahraga";
-
-// import "./App.css";
-// import HitungKaloriPage from "./pages/HitungKaloriPage";
-// import { NormalKalori, RendahKalori, TinggiKalori } from "./components/HitungKalori/Pages/PageRekomendasiMakanan";
-
-
-// import HitungKaloriPage from "./pages/HitungKaloriPage";
-// import {
-//   NormalKalori,
-//   RendahKalori,
-//   TinggiKalori,
-// } from "./components/HitungKalori/Pages/PageRekomendasiMakanan";
-import AboutUs from "./components/AboutUs/AboutUs";
+import HitungKaloriPage from "./pages/HitungKaloriPage";
 import EdukasiPage from "./pages/EdukasiPage";
+
 import Login from "./components/login/Login";
+import Navigation from "./components/Navigation";
+import AboutUs from "./components/AboutUs/AboutUs";
+import {
+  TinggiKalori,
+  NormalKalori,
+  RendahKalori,
+} from "./components/HitungKalori/Pages/PageRekomendasiMakanan";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
       <Navigation />
       <Routes>
-
-        {/* <Route path="/" element={<AdminPage />} />
-        <Route path="/about" element={<About />} />
-
-
         <Route path="/" element={<HomePage />} />
         <Route path="/hitung-kalori" element={<HitungKaloriPage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/edukasi" element={<EdukasiPage />} />
         <Route path="/contact" element={<AdminPage />} />
-        <Route path="/about-us" element={<AboutUs />} />
-
         <Route path="/alldiet" element={<AllDiet />} />
         <Route path="/makanan-all" element={<PageMakananAll />} />
         <Route path="/olahraga-all" element={<PageOlahragaAll />} />
@@ -59,17 +41,9 @@ function App() {
         <Route path="/normal" element={<NormalKalori />} />
         <Route path="/rendah" element={<RendahKalori />} />
         <Route path="/edit-makanan/:uuid" element={<EditMakanan />} />
-        <Route path="/edit-olahraga/:uuid" element={<EditOlahraga />} /> */}
-
-        <Route path="/" element={<HomePage />} />
-        
-        <Route path="/about" element={<About />} />
-        <Route path="/alldiet" element={<AllDiet />} />
-        <Route path="/makanan-all" element={<PageMakananAll />} />
-        <Route path="/olahraga-all" element={<PageOlahragaAll />} />
-        <Route path="/details/:id" element={<DetailsPageDiet />} />
+        <Route path="/edit-olahraga/:uuid" element={<EditOlahraga />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
